c53a212a2f4fafe2f4180ef4b96463d3a6dde7ab
Abdulaziz <almutlaqaziz32@gmail.com>
2022-09-16 19:58:05 -0700

    comment

--- gitexercise.java
+++ gitexercise.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class GitExercise {
-
+// comment for the video
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
